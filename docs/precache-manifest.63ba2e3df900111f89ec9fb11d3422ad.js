0660b31bded6f22102f3ffc668d0acd7d0f73892
Carles Font <carlesfontm@gmail.com>
2021-03-10 11:51:00 +0100

    Docs agregado

---
+++ docs/precache-manifest.63ba2e3df900111f89ec9fb11d3422ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5466a6df53656f9612b2f17816f6ea39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7eaf5c41ad2bf13599c",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7eaf5c41ad2bf13599c",
+    "url": "/static/js/main.defef9d4.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
